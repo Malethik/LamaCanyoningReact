e0c98d639d551344092a226741fc8f39279fbf2c
Daniele Quintiliani <quintiliani.d@gmail.com>
2025-01-11 23:09:53 +0100

    added title to navbar link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,13 +5,19 @@ const NavBar = () => {
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" title="Home">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/item">Inventario</Link>
+          <Link to="/item" title="Inventario">
+            Inventario
+          </Link>
         </li>
         <li>
-          <Link to="/supplier">Fornitori</Link>
+          <Link to="/supplier" title="Fornitori">
+            Fornitori
+          </Link>
         </li>
       </ul>
     </nav>
