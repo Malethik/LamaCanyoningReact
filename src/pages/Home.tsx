03b8bddd3271aa122623d79ac96980a728118865
Daniele Quintiliani <quintiliani.d@gmail.com>
2025-01-14 13:14:04 +0100

    added helpers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,51 +3,8 @@ import ApexChart from "../components/ApexChart";
 import useFetch from "../hooks/useFetch";
 import { Item } from "../model/item";
 import { Order } from "../model/order";
-
-const getMonthlyData = (items: Item[]) => {
-  const monthlyData: { [key: string]: number } = {};
-
-  items.forEach((item) => {
-    const month = new Date(item.createdAt).toLocaleString("default", {
-      month: "short",
-      year: "numeric",
-    });
-
-    if (monthlyData[month]) {
-      monthlyData[month] += item.price * item.quantity; // somma il prezzo * quantità
-    } else {
-      monthlyData[month] = item.price * item.quantity;
-    }
-  });
-
-  return Object.entries(monthlyData)
-    .sort(
-      ([a], [b]) => new Date(`1 ${a}`).getTime() - new Date(`1 ${b}`).getTime()
-    )
-    .map(([, value]) => value);
-};
-const getMonthlyDataForOrders = (orders: Order[]) => {
-  const monthlyData: { [key: string]: number } = {};
-
-  orders.forEach((order) => {
-    const month = new Date(order.createdAt).toLocaleString("default", {
-      month: "short",
-      year: "numeric",
-    });
-
-    if (monthlyData[month]) {
-      monthlyData[month] += order.total;
-    } else {
-      monthlyData[month] = order.total;
-    }
-  });
-
-  return Object.entries(monthlyData)
-    .sort(
-      ([a], [b]) => new Date(`1 ${a}`).getTime() - new Date(`1 ${b}`).getTime()
-    )
-    .map(([, value]) => value);
-};
+import getMonthlyDataItems from "../helper/getMonthlyDataItems";
+import getMonthlyDataForOrders from "../helper/getMonthlyDataOrders";
 
 const Home: React.FC = () => {
   const [expensesData, setExpenses] = useState<number[]>([]);
@@ -71,7 +28,7 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     if (itemsData) {
-      setExpenses(getMonthlyData(itemsData));
+      setExpenses(getMonthlyDataItems(itemsData));
     }
   }, [itemsData]);
 
